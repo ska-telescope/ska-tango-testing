90fac6e6fb59f17aa525d3bb313371b8333f282b
Drew Devereux <drew.devereux@csiro.au>
2022-08-01 04:47:03 +0000

    [LOW-282] multiple devices

--- src/ska_tango_testing/context.py
+++ src/ska_tango_testing/context.py
@@ -1,15 +1,42 @@
 """This module provides support for tango testing contexts."""
-# TODO: So far this module only supports single device contexts.
 
 from types import TracebackType
-from typing import Any, Dict, Optional, Type
+from typing import Any, Dict, List, Optional, Type, Union
 
 import tango
 import tango.server
-from typing_extensions import Literal
+import tango.test_context
+from typing_extensions import Literal, Protocol
 
 
-class TrueTangoContext:
+class _DeviceProxyFactory:  # pylint: disable=too-few-public-methods
+    def __init__(self) -> None:
+        self.factory = tango.DeviceProxy
+
+    def __call__(
+        self, fqdn: str, *args: Any, **kwargs: Any
+    ) -> tango.DeviceProxy:
+        return self.factory(fqdn, *args, **kwargs)
+
+
+DeviceProxy = _DeviceProxyFactory()
+
+
+# pylint: disable-next=too-few-public-methods
+class TangoContextProtocol(Protocol):
+    """Protocol for a tango context."""
+
+    def get_device(self, fqdn: str) -> tango.DeviceProxy:
+        """
+        Return a proxy to a specified device.
+
+        :param fqdn: FQDN of the device
+
+        :return: a proxy to the device
+        """  # noqa: DAR202
+
+
+class TrueTangoContextManager:
     """
     A Tango context in which the device has already been deployed.
 
@@ -17,23 +44,13 @@ class TrueTangoContext:
     want to run tests against it.
     """
 
-    def __init__(self, fqdn: str) -> None:
-        """
-        Initialise a new instance.
-
-        :param fqdn: FQDN of the device under test
-        """
-        self._fqdn = fqdn
-
-    def __enter__(self) -> tango.DeviceProxy:
+    def __enter__(self) -> TangoContextProtocol:
         """
         Enter the context.
 
-        This is called when the "with" syntax is used.
-
-        :return: a proxy to the device under test
+        :return: the context
         """
-        return tango.DeviceProxy(self._fqdn)
+        return self
 
     def __exit__(
         self,
@@ -53,39 +70,74 @@ class TrueTangoContext:
         """
         return False
 
+    # pylint: disable-next=no-self-use
+    def get_device(self, fqdn: str) -> tango.DeviceProxy:
+        """
+        Return a proxy to a specified device.
 
-class TestTangoContext:
+        :param fqdn: FQDN of the device
+
+        :return: a proxy to the device
+        """
+        return tango.DeviceProxy(fqdn)
+
+
+class ThreadedTestTangoContextManager:
     """A lightweight context for testing Tango devices."""
 
-    def __init__(
-        self,
-        device_class: Type[tango.server.Device],
-        properties: Dict[str, Any],
-        process: bool = True,
-    ):
-        """
-        Initialise a new instance.
+    def __init__(self) -> None:
+        """Initialise a new instance."""
+        self._device_info_by_class: Dict[
+            Union[str, Type[tango.server.Device]], List[Dict[str, Any]]
+        ] = {}
+        self._context = None
 
-        :param device_class: class of the device under test
-        :param properties: properties of the device under test.
-        :param process: whether to run the device in a separate process.
-            If False, the device is run in a separate thread.
+    def add_device(
+        self,
+        fqdn: str,
+        device_class: Union[str, Type[tango.server.Device]],
+        **properties: Any,
+    ) -> None:
         """
-        self._context = tango.test_context.DeviceTestContext(
-            device_class,
-            properties=properties,
-            process=process,
-            daemon=True,
+        Add a device to the context managed by this manager.
+
+        :param fqdn: the FQDN of the device to be added
+        :param device_class: the class of the device to be added. This
+            can be the class itself, or its name.
+        :param properties: a dictionary of device properties
+        """
+        self._device_info_by_class.setdefault(device_class, [])
+        self._device_info_by_class[device_class].append(
+            {"name": fqdn, "properties": properties}
         )
 
-    def __enter__(self) -> tango.DeviceProxy:
+    def __enter__(self) -> TangoContextProtocol:
         """
         Enter the context.
 
+        The context is a :py:class:`tango.test_context.MultiDeviceTestContext`,
+        which has a known bug that forces us to patch `tango.DeviceProxy`.
+
         :return: a proxy to the device under test.
         """
+        device_info = [
+            {"class": class_name, "devices": devices}
+            for class_name, devices in self._device_info_by_class.items()
+        ]
+
+        self._context = tango.test_context.MultiDeviceTestContext(
+            device_info,
+            process=False,
+            daemon=True,
+        )
+        assert self._context is not None  # for the type checker
+
+        DeviceProxy.factory = lambda fqdn, *args, **kwargs: tango.DeviceProxy(
+            self._context.get_device_access(fqdn), *args, **kwargs
+        )
+
         self._context.__enter__()
-        return self._context.device
+        return self._context
 
     def __exit__(
         self,
@@ -103,4 +155,9 @@ class TestTangoContext:
         :returns: whether the exception (if any) has been fully handled
             by this method and should be swallowed i.e. not re-raised
         """
-        return self._context.__exit__(exc_type, exception, trace)
+        assert self._context is not None  # for the type checker
+        try:
+            # pylint: disable-next=assignment-from-no-return
+            return self._context.__exit__(exc_type, exception, trace)
+        finally:
+            self._context = None
---
+++ tests/unit/test_context.py
@@ -0,0 +1,100 @@
+"""This module tests the ska_tango_testing version."""
+
+from typing import Optional
+
+import pytest
+import tango.server
+
+from ska_tango_testing.context import (
+    DeviceProxy,
+    ThreadedTestTangoContextManager,
+)
+
+
+class SoloDevice(tango.server.Device):
+    """A basic device for use in testing."""
+
+    Value = tango.server.device_property(dtype=int)
+
+    def init_device(self) -> None:
+        """Initialise this device."""
+        self.get_device_properties()
+
+    @tango.server.attribute(dtype=int)
+    def value(self) -> int:
+        """
+        Return the value of the "value" attribute.
+
+        :return: the value of the "value" attribute.
+        """
+        return self.Value
+
+
+class TwinDevice(tango.server.Device):
+    """A device that interacts with another, for use in testing."""
+
+    Value = tango.server.device_property(dtype=int)
+    Twin = tango.server.device_property(dtype=str)
+
+    def init_device(self) -> None:
+        """Initialise this device."""
+        self.get_device_properties()
+        self._twin_value: Optional[int] = None
+
+    @tango.server.attribute(dtype=int)
+    def value(self) -> int:
+        """
+        Return the value of the "value" attribute.
+
+        :return: the value of the "value" attribute.
+        """
+        return self.Value
+
+    @tango.server.attribute(dtype=int)
+    def twin_value(self) -> int:
+        """
+        Return the value of the "value" attribute.
+
+        :return: the value of the "value" attribute.
+        """
+        if self._twin_value is None:
+            twin = DeviceProxy(self.Twin)
+            # pylint: disable-next=attribute-defined-outside-init
+            self._twin_value = twin.value
+        assert self._twin_value is not None  # for the type checker
+        return self._twin_value
+
+
+class TestThreadedTestTangoContextManager:
+    """Container class for tests of the ThreadedTestTangoContextManager."""
+
+    @pytest.mark.forked  # pylint: disable-next=no-self-use
+    def test_context_manager_with_a_single_device(self) -> None:
+        """Test a context manager with a single device."""
+        context_manager = ThreadedTestTangoContextManager()
+        context_manager.add_device("foo/bar/1", SoloDevice, Value=1)
+
+        with context_manager as context:
+            device = context.get_device("foo/bar/1")
+            assert device.value == 1
+
+    @pytest.mark.forked  # pylint: disable-next=no-self-use
+    def test_context_manager_with_multiple_devices(self) -> None:
+        """Test a context manager with multiple devices that communicate."""
+        context_manager = ThreadedTestTangoContextManager()
+        context_manager.add_device(
+            "foo/bar/1", TwinDevice, Value=1, Twin="foo/bar/2"
+        )
+        context_manager.add_device(
+            "foo/bar/2", TwinDevice, Value=2, Twin="foo/bar/1"
+        )
+
+        with context_manager as context:
+            device_1 = context.get_device("foo/bar/1")
+            device_2 = context.get_device("foo/bar/2")
+
+            assert device_1.value == 1
+            assert device_2.value == 2
+
+            assert device_1.twin_value == 2
+            assert device_2.twin_value == 1
