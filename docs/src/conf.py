0feb167996ed7adaab1670fece43b5ec952bd921
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-05-29 14:37:56 +0200

    REL-1496: make pump-patch-release from 0.6.1 to 0.6.2 (update remaining ref to 0.6.1)

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, CSIRO'
 author = 'Drew Devereux <drew.devereux@csiro.au>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.6.2'
 
 
 # -- General configuration ---------------------------------------------------
