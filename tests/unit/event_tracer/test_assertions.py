e9c152c42f9cf860625d269ff765d832c62f8eeb
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-08-22 11:34:58 +0200

    OMCPT-73: apply suggestion and make _expected_error_message_xxx contracts more explicit (say explicitly that what is returned is a regex and describe better its intended purpose).
    co-authored by: @drewdevereux

--- tests/unit/event_tracer/test_assertions.py
+++ tests/unit/event_tracer/test_assertions.py
@@ -180,12 +180,21 @@ class TestCustomAssertions:
         expected_n_events: int = 1,
         timeout: int | None = None,
     ) -> str:
-        """Generate (a piece of) the error msg when has event assertion fails.
+        """A fragment of a regular expression for error message validation.
+
+        This method returns a regex pattern fragment intended
+        to match the start of an error message when an event assertion fails.
+        It is parametrized with the number of detected events (defaults to
+        0, since it is the most common case), the number of expected events
+        (defaults to 1, since it is the most common case) and the timeout
+        value (defaults to None, since in most of the tests it is not
+        specified).
 
         :param detected_n_events: The number of events detected.
         :param expected_n_events: The number of events expected.
         :param timeout: The timeout value. By default, it is not specified.
-        :return: The error message (or better, the start of it).
+        :return: The regex pattern fragment to match the start of
+            the error message.
         """
         res = rf"(?:Expected to find {expected_n_events} event\(s\) "
         res += "matching the predicate "
@@ -481,14 +490,25 @@ class TestCustomAssertions:
         expected_n_events: int = 1,
         timeout: int | None = None,
     ) -> str:
-        """Generate (a piece of) the err msg when hasnt event assertion fails.
+        """A fragment of a regular expression for hasnt event error message.
+
+        This method returns a regex pattern fragment intended
+        to match the start of an error message when an event assertion fails
+        in the hasnt assertion. It is parametrized with the number of detected
+        events and the (not) expected number of events (both defaults to
+        1, since most of the times you were expecting less than 1 event and
+        instead you found exactly 1 event), and the timeout value (defaults
+        to None, since in most of the tests it is not specified).
 
         :param detected_n_events: The number of events detected.
-        :param expected_n_events: The number of events expected.
+        :param expected_n_events: The number of events expected. It is intended
+            as "less than" expected_n_events, so it defaults to 1 (because
+            most of the times you want no events).
         :param timeout: The timeout value. By default, it is not specified.
-        :return: The error message (or better, the start of it).
+        :return: The regex pattern fragment to match the start of
+            the error message.
         """
-        res = f"(?:Expected to NOT find {expected_n_events} event\\(s\\) "
+        res = rf"(?:Expected to NOT find {expected_n_events} event\(s\) "
         res += "matching the predicate "
 
         if timeout is not None:
