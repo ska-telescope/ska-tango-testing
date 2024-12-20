f6180da8a6153030e8b356a9b19313742e836ea2
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-12-20 15:52:09 +0100

    SST-1022: add unit test to validate the subscription with a device proxy

--- tests/unit/event_tracer/test_subscription_manager.py
+++ tests/unit/event_tracer/test_subscription_manager.py
@@ -5,9 +5,10 @@ This set of tests covers the basic functionality of the
 and correct event handling.
 """
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
+import tango
 from assertpy import assert_that
 
 from ska_tango_testing.integration.event import ReceivedEvent
@@ -16,7 +17,7 @@ from tests.unit.event_tracer.testing_utils.patch_context_devproxy import (
     patch_context_device_proxy,
 )
 
-from .testing_utils import create_eventdata_mock
+from .testing_utils import DeviceProxyMock, create_eventdata_mock
 
 
 @pytest.mark.integration_tracer
@@ -24,8 +25,8 @@ class TestTangoSubscriber:
     """Unit tests for the TangoSubscriber class."""
 
     @staticmethod
-    def test_subscribe_event_adds_subscription() -> None:
-        """Test that subscribing to an event adds a subscription."""
+    def test_subscribe_event_adds_subscription_and_subscribes() -> None:
+        """Subscription adds a subscription and subscribes to the event."""
         with patch_context_device_proxy() as mock_proxy:
             mock_proxy.return_value.subscribe_event.return_value = 1234
             subscriber = TangoSubscriber()
@@ -41,10 +42,42 @@ class TestTangoSubscriber:
             # pylint: disable=protected-access
             subscriber._subscription_ids[mock_proxy.return_value]
         ).described_as("Subscription ID should be stored").contains(1234)
+        assert_that(
+            mock_proxy.return_value.subscribe_event.call_args_list
+        ).described_as("subscribe_event should be called").is_length(1)
+        assert_that(
+            mock_proxy.return_value.subscribe_event.call_args_list[0][0][0]
+        ).described_as(
+            "subscribe_event should be called with the correct attribute name"
+        ).is_equal_to(
+            "test_attr"
+        )
+
+    @staticmethod
+    def test_subscribe_event_passing_instance() -> None:
+        """Subscription works also passing a device instance."""
+        device_name, attribute_name = "test_device", "test_attr"
+        with patch("tango.DeviceProxy", new_callable=DeviceProxyMock):
+            device_proxy = tango.DeviceProxy(device_name)
+            subscriber = TangoSubscriber()
+            callback = MagicMock()
+
+            subscriber.subscribe_event(device_proxy, attribute_name, callback)
+
+        assert_that(device_proxy.subscribe_event.call_args_list).described_as(
+            "subscribe_event should be called"
+        ).is_length(1)
+        assert_that(
+            device_proxy.subscribe_event.call_args_list[0][0][0]
+        ).described_as(
+            "subscribe_event should be called with the correct attribute name"
+        ).is_equal_to(
+            attribute_name
+        )
 
     @staticmethod
     def test_unsubscribe_all_removes_all_subscriptions() -> None:
-        """Test that unsubscribing from all removes all subscriptions."""
+        """Unsubscribe method removes all subscriptions."""
         with patch_context_device_proxy() as mock_proxy:
             mock_proxy.return_value.subscribe_event.return_value = 1234
             subscriber = TangoSubscriber()
@@ -72,7 +105,7 @@ class TestTangoSubscriber:
 
     @staticmethod
     def test_callback_is_called_with_received_event() -> None:
-        """Test that the callback is called with a ReceivedEvent."""
+        """Passed callback function is called with ReceivedEvent instance."""
         subscriber = TangoSubscriber()
         callback = MagicMock()
         event_data = create_eventdata_mock("test/device/1", "test_attr", 42)
