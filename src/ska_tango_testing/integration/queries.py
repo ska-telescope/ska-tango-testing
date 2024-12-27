ed47f0c8110a469300f70fc1699f268e463a59bb
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-12-27 15:55:07 +0100

    SST-1022: implement a query decorator class that wraps another query and fail early if a certain fail condition is met

--- src/ska_tango_testing/integration/queries.py
+++ src/ska_tango_testing/integration/queries.py
@@ -50,3 +50,62 @@ class NEventsMatchQuery(EventQuery):
         for event in events:
             if self.predicate(event) and event not in self.matching_events:
                 self.matching_events.append(event)
+
+
+class QueryWithFailCondition(EventQuery):
+    """A query that wraps another query and stops early if a condition is met.
+
+    This query wraps another query and stops the evaluation early if a given
+    stop condition is met. The stop condition is a function that takes an event
+    as input and returns True if the query should stop evaluating events.
+    Each new event is evaluated by the stop condition before being passed to
+    the wrapped query.
+
+    Subscribe just this query to the event storage to evaluate it, not
+    the wrapped query. The timeout of the wrapped query will be ignored.
+    """
+
+    def __init__(
+        self,
+        wrapped_query: EventQuery,
+        stop_condition: Callable[[ReceivedEvent], bool],
+        timeout: SupportsFloat = 0.0,
+    ) -> None:
+        """Initialize the query with the wrapped query and stop condition.
+
+        :param wrapped_query: The query to wrap.
+        :param stop_condition: A function that takes an event
+            as input and returns True if the stop condition is met.
+        :param timeout: The timeout for the query in seconds. Defaults to 0.
+        """
+        super().__init__(timeout)
+        self.wrapped_query = wrapped_query
+        self.stop_condition = stop_condition
+        self.failed_event: ReceivedEvent | None = None
+
+    def _succeeded(self) -> bool:
+        """Check if the query succeeded.
+
+        :return: True if the query succeeded, False otherwise.
+        """
+        return self.wrapped_query.succeeded() and self.failed_event is None
+
+    def _evaluate_events(self, events: List[ReceivedEvent]) -> None:
+        """Evaluate the query based on the current events.
+
+        :param events: The updated list of events.
+        """
+        for event in events:
+            if self.stop_condition(event):
+                self.failed_event = event
+                self._timeout_signal.set()
+                return
+
+        self.wrapped_query.on_events_change(events)
+
+    def _is_stop_criteria_met(self) -> bool:
+        """Stop the query if it succeeded or if the stop condition is met.
+
+        :return: True if the query succeeded or if the stop condition is met.
+        """
+        return self._succeeded() or self.failed_event is not None
--- tests/unit/event_tracer/test_queries.py
+++ tests/unit/event_tracer/test_queries.py
@@ -1,8 +1,12 @@
-"""Unit tests for :py:class:`NEventsMatchQuery`.
+"""Unit tests for EventQuery concrete implementations.
+
+Currently, the tested classes are:
+:py:class:`NEventsMatchQuery` and :py:class:`QueryWithFailCondition`.
 
 This set of tests covers the basic functionality of the
-:py:class:`NEventsMatchQuery` class, focusing on matching events
-through a predicate and succeeding when a target number of events is reached.
+:py:class:`NEventsMatchQuery` and :py:class:`QueryWithFailCondition` classes,
+focusing on matching events through a predicate and succeeding when a target
+number of events is reached, and stopping early if a stop condition is met.
 """
 
 import time
@@ -12,7 +16,10 @@ from assertpy import assert_that
 
 from ska_tango_testing.integration.event_query import EventQueryStatus
 from ska_tango_testing.integration.event_storage import EventStorage
-from ska_tango_testing.integration.queries import NEventsMatchQuery
+from ska_tango_testing.integration.queries import (
+    NEventsMatchQuery,
+    QueryWithFailCondition,
+)
 
 from .test_events_query import TestEventQuery
 from .testing_utils.received_event_mock import create_test_event
@@ -139,3 +146,196 @@ class TestNEventsMatchQuery:
         assert_that(query.matching_events).described_as(
             "Query should not have collected all expected events"
         ).is_length(1)
+
+
+@pytest.mark.integration_tracer
+class TestQueryWithFailCondition:
+    """Unit tests for the QueryWithFailCondition class."""
+
+    @staticmethod
+    def test_query_succeeds_when_event_matches() -> None:
+        """Test that the query succeeds when an event matches."""
+        storage = EventStorage()
+        wrapped_query = NEventsMatchQuery(
+            predicate=lambda e: e.has_device("test/device/1")
+            and e.has_attribute("test_attr")
+            and e.attribute_value == 42,
+        )
+        query = QueryWithFailCondition(
+            wrapped_query=wrapped_query,
+            stop_condition=lambda e: False,
+        )
+        # this event will match the query
+        event = create_test_event()
+        storage.store(event)
+
+        storage.subscribe(query)
+        query.evaluate()
+
+        assert_that(query.succeeded()).described_as(
+            "Query should succeed when an event matches"
+        ).is_true()
+        assert_that(query.status()).described_as(
+            "Query status should be SUCCEEDED when an event matches"
+        ).is_equal_to(EventQueryStatus.SUCCEEDED)
+        assert_that(wrapped_query.succeeded()).described_as(
+            "Wrapped query should succeed too"
+        ).is_true()
+        assert_that(wrapped_query.matching_events).described_as(
+            "Wrapped query should collect the matching event"
+        ).is_length(1)
+        assert_that(wrapped_query.matching_events[0]).described_as(
+            "Wrapped query should collect the correct event"
+        ).is_equal_to(event)
+
+    @staticmethod
+    def test_query_stop_condition_makes_the_test_fail() -> None:
+        """Test that the query fails when the stop condition is met."""
+        storage = EventStorage()
+        wrapped_query = NEventsMatchQuery(
+            predicate=lambda e: e.has_device("test/device/1")
+            and e.has_attribute("test_attr")
+            and e.attribute_value == 42,
+        )
+        query = QueryWithFailCondition(
+            wrapped_query=wrapped_query,
+            stop_condition=lambda e: e.has_device("test/device/2"),
+        )
+        # this event could match the query but it will not be evaluated
+        event = create_test_event()
+        storage.store(event)
+        # this event will trigger the stop condition
+        fail_event = create_test_event(device_name="test/device/2")
+        storage.store(fail_event)
+
+        storage.subscribe(query)
+        query.evaluate()
+
+        assert_that(query.succeeded()).described_as(
+            "Query should fail when the stop condition is met"
+        ).is_false()
+        assert_that(query.status()).described_as(
+            "Query status should be FAILED when the stop condition is met"
+        ).is_equal_to(EventQueryStatus.FAILED)
+        assert_that(wrapped_query.matching_events).described_as(
+            "Wrapped query should not collect any events"
+        ).is_empty()
+        assert_that(query.failed_event).described_as(
+            "Query should store the event that caused the failure"
+        ).is_equal_to(fail_event)
+
+    @staticmethod
+    def test_query_succeeds_with_delayed_event() -> None:
+        """The query should succeed when an event is delayed but matches."""
+        storage = EventStorage()
+        wrapped_query = NEventsMatchQuery(
+            predicate=lambda e: e.has_device("test/device/1")
+            and e.has_attribute("test_attr")
+            and e.attribute_value == 42,
+            timeout=0,  # this timeout will be ignored
+        )
+        query = QueryWithFailCondition(
+            wrapped_query=wrapped_query,
+            stop_condition=lambda e: False,
+            timeout=2,  # this timeout will be used
+        )
+        # this event will make the query succeed
+        event = create_test_event()
+        TestEventQuery.delayed_store_event(storage, event, delay=1)
+        # this event could potentially make the query fail, but it will
+        # arrive too late and the query will have already succeeded
+        fail_event = create_test_event(device_name="test/device/2")
+        TestEventQuery.delayed_store_event(storage, fail_event, delay=1.5)
+
+        storage.subscribe(query)
+        query.evaluate()
+        time.sleep(1)
+
+        assert_that(wrapped_query.succeeded()).described_as(
+            "Wrapped query should succeed"
+        ).is_true()
+        assert_that(query.succeeded()).described_as(
+            "Query should succeed when an event is received during evaluation"
+        ).is_true()
+        assert_that(query.status()).described_as(
+            "Query status should be SUCCEEDED when an event "
+            "is received during evaluation"
+        ).is_equal_to(EventQueryStatus.SUCCEEDED)
+        assert_that(wrapped_query.matching_events).described_as(
+            "Wrapped query should collect the matching event"
+        ).is_length(1)
+        assert_that(wrapped_query.matching_events[0]).described_as(
+            "Wrapped query should collect the correct event"
+        ).is_equal_to(event)
+
+    @staticmethod
+    def test_query_fails_with_delayed_stop_event() -> None:
+        """The query should fail when the stop condition is met (delayed)."""
+        storage = EventStorage()
+        wrapped_query = NEventsMatchQuery(
+            predicate=lambda e: e.has_device("test/device/1")
+            and e.has_attribute("test_attr")
+            and e.attribute_value == 42,
+            timeout=2,  # this timeout will be ignored
+        )
+        query = QueryWithFailCondition(
+            wrapped_query=wrapped_query,
+            stop_condition=lambda e: e.has_device("test/device/2"),
+            timeout=1,  # this timeout will be used
+        )
+        # this event will trigger the stop condition
+        fail_event = create_test_event(device_name="test/device/2")
+        TestEventQuery.delayed_store_event(storage, fail_event, delay=1)
+        # this event could match the query but it will not be evaluated
+        # because the stop condition will be met first
+        event = create_test_event()
+        TestEventQuery.delayed_store_event(storage, event, delay=1.5)
+
+        storage.subscribe(query)
+        query.evaluate()
+
+        assert_that(query.succeeded()).described_as(
+            "Query should fail when the stop condition "
+            "is met with a delayed event"
+        ).is_false()
+        assert_that(query.status()).described_as(
+            "Query status should be FAILED when the stop condition "
+            "is met with a delayed event"
+        ).is_equal_to(EventQueryStatus.FAILED)
+        assert_that(wrapped_query.matching_events).described_as(
+            "Wrapped query should not collect any events"
+        ).is_empty()
+        assert_that(query.failed_event).described_as(
+            "Query should store the event that caused the failure"
+        ).is_equal_to(fail_event)
+
+    @staticmethod
+    def test_query_timeout() -> None:
+        """The query times out when no event matches within timeout."""
+        storage = EventStorage()
+        wrapped_query = NEventsMatchQuery(
+            predicate=lambda e: e.has_device("test/device/1")
+            and e.has_attribute("test_attr")
+            and e.attribute_value == 42,
+            timeout=1,
+        )
+        query = QueryWithFailCondition(
+            wrapped_query=wrapped_query,
+            stop_condition=lambda e: False,
+        )
+
+        storage.subscribe(query)
+        query.evaluate()
+
+        assert_that(query.succeeded()).described_as(
+            "Query should fail when it times out"
+        ).is_false()
+        assert_that(query.status()).described_as(
+            "Query status should be FAILED when it times out"
+        ).is_equal_to(EventQueryStatus.FAILED)
+        assert_that(wrapped_query.matching_events).described_as(
+            "Wrapped query should not collect any events"
+        ).is_empty()
+        assert_that(query.failed_event).described_as(
+            "Query should not store any event that caused the failure"
+        ).is_none()
