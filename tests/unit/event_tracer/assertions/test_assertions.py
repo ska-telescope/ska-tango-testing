4397e11f25c256ab90aaec7b6361faaa16cd7f6f
Emanuele Lena <emanuele.lena@designcoaching.net>
2025-01-08 16:39:59 +0100

    SST-1022: separate assertions are exposed tests

--- tests/unit/event_tracer/assertions/test_assertions.py
+++ tests/unit/event_tracer/assertions/test_assertions.py
@@ -23,34 +23,6 @@ class TestCustomAssertions:
     partial matches, correct evaluation of previous event and so on.
     """
 
-    @staticmethod
-    def _assert_exposes(tracer: TangoEventTracer, assertion_name: str) -> None:
-        """Check that a custom assertion is accessible and callable.
-
-        :param tracer: The `TangoEventTracer` instance.
-        :param assertion_name: The name of the custom assertion.
-        """
-        custom = getattr(assert_that(tracer), assertion_name, None)
-        assert_that(custom).described_as(
-            f"Expected the custom assertion '{assertion_name}' "
-            "to be exposed."
-        ).is_not_none()
-        assert_that(callable(custom)).described_as(
-            f"Expected the custom assertion '{assertion_name}' "
-            "to be a callable."
-        ).is_true()
-
-    def test_assert_that_exposes_custom_assertions(
-        self, tracer: TangoEventTracer
-    ) -> None:
-        """The custom assertions are exposed.
-
-        :param tracer: The `TangoEventTracer` instance.
-        """
-        self._assert_exposes(tracer, "has_change_event_occurred")
-        self._assert_exposes(tracer, "hasnt_change_event_occurred")
-        self._assert_exposes(tracer, "within_timeout")
-
     # ##########################################################
     # Tests: assert has change events occurred
 
