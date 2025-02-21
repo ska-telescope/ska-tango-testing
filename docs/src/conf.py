72c881440e0328cbf52c57bd9ade5778233ec383
Emanuele Lena <emanuele.lena@designcoaching.net>
2025-02-21 08:41:39 +0100

    REL-1974: bump 0.8.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, CSIRO"
 author = "Drew Devereux <drew.devereux@csiro.au>"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.3"
+release = "0.8.0"
 
 
 # -- General configuration ---------------------------------------------------
--- src/ska_tango_testing/__init__.py
+++ src/ska_tango_testing/__init__.py
@@ -1,3 +1,3 @@
 """This package provides test harness for testing of SKA Tango devices."""
 
-__version__ = "0.7.3"
+__version__ = "0.8.0"
