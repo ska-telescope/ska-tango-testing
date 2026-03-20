1fbae330f468fb5538b38f2ffa4bcf1814b679a2
SKAJohanVenter <aventer@ska.ac.za>
2026-03-20 12:57:11 +0200

    KAR-2116 mypy check workarounds

--- src/ska_tango_testing/integration/event/subscriber.py
+++ src/ska_tango_testing/integration/event/subscriber.py
@@ -232,7 +232,9 @@ class TangoSubscriber:
         # create the device proxy if needed (using the provided factory
         # or the default one)
         if isinstance(device_name, str):
-            dev_factory = dev_factory or ska_tango_testing.context.DeviceProxy  # type: ignore
+            dev_factory = (
+                dev_factory or ska_tango_testing.context.DeviceProxy
+            )  # type: ignore
             return dev_factory(device_name)
 
         # If the device_name is already a DeviceProxy, return it
