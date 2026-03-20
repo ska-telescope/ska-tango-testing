33fb4114d8923a223ec1a30edeebe54a5f562cb9
SKAJohanVenter <aventer@ska.ac.za>
2026-03-20 09:08:07 +0200

    KAR-2116 Open up pytango and numpy version to allow for pytango upgrades in other projects.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, CSIRO"
 author = "Drew Devereux <drew.devereux@csiro.au>"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
